18ef37dad0688411e2d1015dd4da61e83309172a
ayushi-das <ayushidas503@gmail.com>
2022-10-13 02:09:19 +0530

    Error Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './shared/component/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,9 @@ const routes: Routes = [
     path:'details',
     loadChildren:()=>import('./details/details.module').then(m=>m.DetailsModule)
 
+  },
+  {
+    path:'**',component:PageNotFoundComponent
   }
 ];
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FilterPipe } from './pipe/filter.pipe';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import { RouterModule } from '@angular/router';
 
 
 
 
 @NgModule({
-  declarations: [FilterPipe],
+  declarations: [FilterPipe, PageNotFoundComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
 
   ],
   exports:[FilterPipe]
