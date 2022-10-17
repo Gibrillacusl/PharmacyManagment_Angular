0cb538f4e7a94b5ec1ab747ff498829c92f28817
ayushi-das <ayushidas503@gmail.com>
2022-10-18 01:18:20 +0530

    Excel

--- src/app/dashboard/sales-report/sales-report.component.ts
+++ src/app/dashboard/sales-report/sales-report.component.ts
@@ -1,8 +1,10 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import * as FileSaver from 'file-saver'
 import { ToastrService } from 'ngx-toastr';
 import { Order } from 'src/app/models/order';
 import { OrderService } from 'src/app/services/order.service';
+//import { jsPDF } from "jspdf";
 
 @Component({
   selector: 'app-sales-report',
@@ -48,5 +50,43 @@ export class SalesReportComponent implements OnInit {
 
 
   }
+//   exportPdf() {
+//     import("jspdf").then(jsPDF => {
+//         import("jspdf-autotable").then(x => {
+//             const doc = new jsPDF.default(0,0);
+//             doc.autoTable('', this.orderList);
+//             doc.save('products.pdf');
+//         })
+//     })
+// }
+exportExcel() {
+  let filteredOrder:any[]=[];
+  this.orderList.forEach(data=>{
+    filteredOrder.push(
+    {'Order Id':data.orderId,
+    'Order Date':data.orderDate,
+    'PickedUp Status':data.isPickedUp,
+    'Toatal Amount':data.amount,
+    'Items':data.count
+    }
 
+
+    )
+  })
+  import("xlsx").then(xlsx => {
+      const worksheet = xlsx.utils.json_to_sheet(filteredOrder);
+      const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+      const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+      this.saveAsExcelFile(excelBuffer, "products");
+  });
 }
+saveAsExcelFile(buffer: any, fileName: string): void {
+  let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+  let EXCEL_EXTENSION = '.xlsx';
+  const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE
+  });
+  FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+}
+}
+
